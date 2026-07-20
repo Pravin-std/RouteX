8bb5ce63ddcd54b44cd48fc99f0d57b621295d57
pravin-std <pravinstark456@gmail.com>
2026-07-20 19:43:00 +0530

    feat: initialize GoRouter with login page route

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,12 +1,12 @@
 import 'package:go_router/go_router.dart';
-import '../../features/home_placeholder_page.dart';
+import '../../features/authentication/presentation/pages/login_page.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const HomePlaceholderPage(),
+      builder: (context, state) => const LoginPage(),
     ),
   ],
 );
